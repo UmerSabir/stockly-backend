455793619686889b5a423e90962e81e6d9e8a0c7
ucubebros <ucubebros@gmail.com>
2026-04-30 13:46:32 +0500

    fix: load environment variables first before app initialization

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
-import app from "./app.js";
-import connectDB from "./config/db.js";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { validateEnv } from "./utils/validateEnv.js";
+import connectDB from "./config/db.js";
+import app from "./app.js";
+
+// MUST load env variables FIRST before anything else
+dotenv.config();
+validateEnv();
 
 const PORT = process.env.PORT || 5000;
 
+// Connect to database after env variables are loaded
 connectDB();
-dotenv.config();
-validateEnv();
 
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
